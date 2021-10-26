da8a470970434394153afd61f18ef5fed754dccf
Dev Kumar <devskumar99@gmail.com>
2021-10-25 20:06:03 -0400

    Update app.R

--- farms/farms_api/app.R
+++ farms/farms_api/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 #install.packages("DT")
 #install.packages("RMySQL")
@@ -41,7 +32,9 @@ dbDisconnect(farms_db)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ), 
    # Application title
    titlePanel("Farm Database"),
    
